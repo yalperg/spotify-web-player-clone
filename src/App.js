14066949d5cfdc8fb37286a61c63fa44dd7965c1
Yunus Alper Göl <yunus.alper.gl@gmail.com>
2021-09-26 17:03:58 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import { BrowserRouter as Router } from 'react-router-dom'
+import Sidebar from 'components/sidebar'
+
+function App() {
+  return (
+    <Router>
+        <Sidebar />
+    </Router>
+  )
+}
+
+export default App
