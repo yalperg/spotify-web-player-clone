38ae2bea4f72983406a9c23175c6d23a29cfd296
Yunus Alper Göl <yunus.alper.gl@gmail.com>
2021-09-28 23:54:03 +0300

    Make sidebar resizeable

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import { BrowserRouter as Router } from 'react-router-dom'
 import Sidebar from 'components/sidebar/sidebar'
+import ResizeBar from 'components/sidebar/resizebar'
 
 function App() {
   return (
     <Router>
+      <ResizeBar>
         <Sidebar />
+      </ResizeBar>
     </Router>
   )
 }
