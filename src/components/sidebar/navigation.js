e9d24298b76d71c3ca3231aebe1c7af9850fce52
Yunus Alper Göl <yunus.alper.gl@gmail.com>
2021-09-30 10:11:35 +0300

    Change isActive in router link into loaction.pathname because of the 'Warning: Cannot update a component' warning

--- src/components/sidebar/navigation.js
+++ src/components/sidebar/navigation.js
@@ -1,5 +1,4 @@
-import { useState } from 'react'
-import { NavLink } from 'react-router-dom'
+import { NavLink, useLocation } from 'react-router-dom'
 import styles from 'assets/styles/navigation.module.scss'
 import {
   HomeIcon,
@@ -11,7 +10,7 @@ import {
 } from 'assets/icons/icons'
 
 const Navigation = () => {
-  const [activeIcon, setActiveIcon] = useState(0)
+  const location = useLocation()
 
   return (
     <nav className={styles.nav}>
@@ -20,25 +19,17 @@ const Navigation = () => {
           <NavLink
             exact
             to='/'
-            activeClassName={styles.active}
-            isActive={(match, location) => {
-              match && setActiveIcon(0)
-              return match
-            }}>
-            <span>{activeIcon === 0 ? <HomeIconActive /> : <HomeIcon />}</span>
+            activeClassName={styles.active}>
+            <span>{location.pathname === '/' ? <HomeIconActive /> : <HomeIcon />}</span>
             Home
           </NavLink>
         </li>
         <li>
           <NavLink
             to='/search'
-            activeClassName={styles.active}
-            isActive={(match, location) => {
-              match && setActiveIcon(1)
-              return match
-            }}>
+            activeClassName={styles.active}>
             <span>
-              {activeIcon === 1 ? <SearchIconActive /> : <SearchIcon />}
+              {location.pathname === '/search' ? <SearchIconActive /> : <SearchIcon />}
             </span>
             Search
           </NavLink>
@@ -46,13 +37,9 @@ const Navigation = () => {
         <li>
           <NavLink
             to='/collection/playlists'
-            activeClassName={styles.active}
-            isActive={(match, location) => {
-              match && setActiveIcon(2)
-              return match
-            }}>
+            activeClassName={styles.active}>
             <span>
-              {activeIcon === 2 ? <CollectionIconActive /> : <CollectionIcon />}
+              {location.pathname === '/collection/playlists' ? <CollectionIconActive /> : <CollectionIcon />}
             </span>
             Your Library
           </NavLink>
