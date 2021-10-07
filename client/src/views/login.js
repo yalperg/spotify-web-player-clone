843ca420aa76a16e599ee5ed57320cb30ed77533
Yunus Alper Göl <yunus.alper.gl@gmail.com>
2021-10-07 20:12:35 +0300

    Remove auth url from login page to .env file

--- client/src/views/login.js
+++ client/src/views/login.js
@@ -2,12 +2,8 @@ import dotenv from 'dotenv'
 
 dotenv.config()
 
-const redirect_uri =  'http://localhost:3000'
-
-const AUTH_URL = `https://accounts.spotify.com/authorize?client_id=${process.env.REACT_APP_CLIENT_ID}&response_type=code&redirect_uri=${redirect_uri}&scope=user-read-private%20user-top-read%20playlist-read-private%20playlist-read-collaborative%20user-read-recently-played`
-
 const Login = () => {
-  return ( <a href={AUTH_URL}>Login with Spotify</a> )
+  return ( <a href={process.env.REACT_APP_AUTH_URL}>Login with Spotify</a> )
 }
  
 export default Login
