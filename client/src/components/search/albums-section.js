c31c58e531b00a54101d5e61dc31a755d0bfeda6
Yunus Alper Göl <yunus.alper.gl@gmail.com>
2021-10-16 16:25:42 +0300

    Search Limit

--- client/src/components/search/albums-section.js
+++ client/src/components/search/albums-section.js
@@ -1,23 +1,39 @@
+import { useState } from 'react'
 import Card from 'components/card'
 import styles from 'assets/styles/section.module.scss'
 
 const AlbumsSection = ({ title, items }) => {
+  const [limit, setLimit] = useState(7)
+
+  const onClickHandler = () => {
+    if(limit === 7) {
+      setLimit(20)
+    } else {
+      setLimit(7)
+    }
+  } 
+
   return (
     <section className={styles.section}>
-      <h1>{title}</h1>
+      <div className={styles.title}>
+        <h1>{title}</h1>
+        <span onClick={onClickHandler}>{limit === 7 ? 'See More' : 'See Less'}</span>
+      </div>
       <div className={`${styles.cards} ${styles.medium}`}>
-        {items.map(item => {
-          return (
-            <Card
-              key={item.id}
-              id={item.id}
-              image={item.images[0]?.url}
-              title={item.name}
-              creator={item.artists.map(artist => artist.name).join(', ')}
-              size='medium'
-            />
-          )
-        })}
+        {items
+          .filter((item, index) => index < limit)
+          .map(item => {
+            return (
+              <Card
+                key={item.id}
+                id={item.id}
+                image={item.images[0]?.url}
+                title={item.name}
+                creator={item.artists.map(artist => artist.name).join(', ')}
+                size='medium'
+              />
+            )
+          })}
       </div>
     </section>
   )
