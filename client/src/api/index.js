dda3cc139e11fd35425b12c44e32089faef6599e
Yunus Alper Göl <yunus.alper.gl@gmail.com>
2023-03-14 19:38:18 +0300

    refactor(client): change axios instance name as a spotifyApi

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,37 +1,37 @@
-import instance from './axiosConfig';
+import spotifyApi from './spotifyApi';
 
 export const fecthUser = () => {
-  return instance.get('me/');
+  return spotifyApi.get('me/');
 };
 
 export const fetchUsersPlaylists = () => {
-  return instance.get('me/playlists');
+  return spotifyApi.get('me/playlists');
 };
 
 export const fetchUsersTops = type => {
-  return instance.get(`me/top/${type}?limit=50&time_range=long_term`);
+  return spotifyApi.get(`me/top/${type}?limit=50&time_range=long_term`);
 };
 
 export const fetchFeatured = () => {
-  return instance.get(`browse/featured-playlists`);
+  return spotifyApi.get(`browse/featured-playlists`);
 };
 
 export const fetchProfile = id => {
-  return instance.get(`users/${id}`);
+  return spotifyApi.get(`users/${id}`);
 };
 
 export const fetchProfilePlaylists = id => {
-  return instance.get(`users/${id}/playlists`);
+  return spotifyApi.get(`users/${id}/playlists`);
 };
 
 export const search = query => {
-  return instance.get(`search?q=${query}&type=artist,album,track`);
+  return spotifyApi.get(`search?q=${query}&type=artist,album,track`);
 };
 
 export const fetchRecentTrack = () => {
-  return instance.get(`me/player/recently-played?limit=1`);
+  return spotifyApi.get(`me/player/recently-played?limit=1`);
 };
 
 export const fetchPlaylist = id => {
-  return instance.get(`playlists/${id}`);
+  return spotifyApi.get(`playlists/${id}`);
 };
--- client/src/api/spotifyApi.js
+++ client/src/api/spotifyApi.js
@@ -2,11 +2,11 @@ import axios from 'axios';
 
 const accessToken = sessionStorage.getItem('access_token');
 
-const instance = axios.create({
+const spotifyApi = axios.create({
   baseURL: 'https://api.spotify.com/v1/',
   headers: {
     Authorization: `Bearer ${accessToken}`,
   },
 });
 
-export default instance;
+export default spotifyApi;
